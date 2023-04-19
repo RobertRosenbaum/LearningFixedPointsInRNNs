7cdad716b3031a63474d0dc540993a148174ca64
VICKCUL <renjun.zhu@berkeley.edu>
2023-04-19 00:32:49 -0400

    some change
    plots and training

--- Create_Plots.py
+++ Create_Plots.py
@@ -3,36 +3,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def CreatePlots(Training_Stats):
-
     print('Making plots.')
-
+    LearningRates = Training_Stats['LearningRates']
     Losses = Training_Stats['TrainingLosses']
     Accuracies = Training_Stats['TrainingAccuracies']
     TestLosses = Training_Stats['TestLosses']
     TestAccuracies = Training_Stats['TestAccuracies']
+    Jacobian = Training_Stats['Jacobian']
 
+    # plt.figure()
+    # plt.plot(Losses.T)
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Training Loss")
+    # plt.ylim([0, 2.5])
+
+    # Plot training losses
     plt.figure()
-    plt.plot(Losses.T)
+    for kk in range(len(LearningRates)):
+        plt.plot(Losses[kk].T, label=r'$\eta = $' + str(LearningRates[kk]))
     plt.xlabel("Iteration")
     plt.ylabel("Training Loss")
-    plt.ylim([0, 2.5])
+    plt.ylim([0, 3])
+    plt.legend()
+    plt.show()
 
+    # plt.figure()
+    # plt.plot(TestLosses.T)
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Test Loss")
+    # plt.ylim([0, 2.5])
+
+    # Plot testing losses
     plt.figure()
-    plt.plot(TestLosses.T)
+    for kk in range(len(LearningRates)):
+        plt.plot(TestLosses[kk].T, label=r'$\eta = $' + str(LearningRates[kk]))
     plt.xlabel("Iteration")
     plt.ylabel("Test Loss")
-    plt.ylim([0, 2.5])
+    plt.ylim([0, 3])
+    plt.legend()
+    plt.show()
 
+    # plt.figure()
+    # plt.plot(100 * Accuracies.T)
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Training Accuracy (%)")
+    # plt.ylim([0, 100])
+
+    # Plot training accuracy
     plt.figure()
-    plt.plot(100 * Accuracies.T)
+    for kk in range(len(LearningRates)):
+        plt.plot(100 * Accuracies[kk].T, label=r'$\eta = $' + str(LearningRates[kk]))
     plt.xlabel("Iteration")
     plt.ylabel("Training Accuracy (%)")
     plt.ylim([0, 100])
+    plt.legend()
+    plt.show()
 
+    # plt.figure()
+    # plt.plot(100 * TestAccuracies.T)
+    # plt.xlabel("Iteration")
+    # plt.ylabel("Test Accuracy (%)")
+    # plt.ylim([0, 100])
+
+    # Plot testing accuracy
     plt.figure()
-    plt.plot(100 * TestAccuracies.T)
+    for kk in range(len(LearningRates)):
+        plt.plot(100 * TestAccuracies[kk].T, label=r'$\eta = $' + str(LearningRates[kk]))
     plt.xlabel("Iteration")
     plt.ylabel("Test Accuracy (%)")
     plt.ylim([0, 100])
-
+    plt.legend()
     plt.show()
+
+    # Plot Jacobian
+    plt.figure()
+    for kk in range(len(LearningRates)):
+      plt.plot(np.real(Jacobian[kk]),np.imag(Jacobian[kk]),'.', label=r'$\eta = $' + str(LearningRates[kk]))
+      plt.title('Eigenvalues of W after learning')
+      plt.legend()
+      plt.show()
--- Train_MNIST_Model.py
+++ Train_MNIST_Model.py
@@ -70,6 +70,9 @@ def Train_MNIST_Model(LearningRates, Get_Model, readout_matrix, train_batch_size
     Losses = np.zeros((len(LearningRates), total_num_steps))
     Accuracies = np.zeros((len(LearningRates), total_num_steps))
 
+    # Store Jacobian
+    Jacobian = []
+
     t1 = tm()  # Start the timer
     for kk in range(len(LearningRates)):
         torch.manual_seed(seed)
@@ -120,6 +123,11 @@ def Train_MNIST_Model(LearningRates, Get_Model, readout_matrix, train_batch_size
                                                                                  num_epochs, Loss.item(),
                                                                                  100 * Accuracies[kk, j - 1],tm()-t1),flush=True)
 
+    # V: Compute eigenvalues of Jacobian
+    with torch.no_grad():
+        lam, _ = np.linalg.eig((model.WT).cpu().numpy())
+        Jacobian.append(lam)
+
     TrainingTime = tm() - t1
     print("Training time =", TrainingTime, "sec =", TrainingTime / (len(LearningRates) * num_epochs), "sec/epoch =",
           TrainingTime / (len(LearningRates) * total_num_steps), "sec/step")
@@ -136,7 +144,13 @@ def Train_MNIST_Model(LearningRates, Get_Model, readout_matrix, train_batch_size
 
     torch.cuda.empty_cache()  # This helps avoid out-of-memory errors
 
-    Training_Stats = {'TrainingLosses':Losses, 'TrainingAccuracies':Accuracies, 'TestLosses':TestLosses, 'TestAccuracies':TestAccuracies, 'TrainingTime':TrainingTime}
+    Training_Stats = {'LearningRates':LearningRates,
+                      'TrainingLosses':Losses,
+                      'TrainingAccuracies':Accuracies,
+                      'TestLosses':TestLosses,
+                      'TestAccuracies':TestAccuracies,
+                      'TrainingTime':TrainingTime,
+                      'Jacobian': Jacobian}
 
 
     return model,Training_Stats
