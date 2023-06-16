e1288680e72ee314f1c2bf9c42b36b3cc5e36d30
RobertRosenbaum <rjrosenbaum@gmail.com>
2023-06-16 13:19:13 +0200

    Modified default hyperparams: Smaller batch size.

--- Shared_Hyperparams.py
+++ Shared_Hyperparams.py
@@ -14,8 +14,8 @@ eta=.01
 
 # Number of epochs and batch sizes
 num_epochs = 3        # Number of times to go through training data
-train_batch_size = 1024      # Batch size to use with training data
-test_batch_size = 1024  # Batch size to use for test data
+train_batch_size = 512      # Batch size to use with training data
+test_batch_size = 512  # Batch size to use for test data
 
 # Spectral radius of initial hidden connectivity matrix
 rho=0.5
